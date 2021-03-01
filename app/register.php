4b4c9bacaa165f7dc2c7a347a40f03b166377e5e
derekvancan1 <dmv7@njit.edu>
2021-03-01 02:18:25 +0000

    added a basic submit form

--- app/register.php
+++ app/register.php
@@ -3,8 +3,11 @@ require(__DIR__."/MQPublish.inc.php");
 session_start();
 ?>
 <form method="POST">
-<input type="text" name="username"/>
-<input type="password" name="password"/>
+<input type="email" placeholder="email" name="email"/>
+<input type="text" placeholder="username" name="username"/>
+<input type="text" placeholder="first name" name="first name"/>
+<input type="text" placeholder="last name" name="last name"/>
+<input type="password" placeholder="password" name="password"/>
 <input type="submit" name="submit" value="Register"/>
 </form>
 
@@ -13,6 +16,7 @@ if(isset($_POST["submit"])){
 	$username = $_POST["username"];
 	$password = $_POST["password"];
 	//TODO validate
+	
 
 	//calls function from MQPublish.inc.php to communicate with MQ
 	$response = register($username, $password);
