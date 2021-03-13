c16cac792af7785d6ebfa8817fc100d10fa4e063
reybarte <reynaldbarte@gmail.com>
2021-03-12 21:21:33 -0500

    feature-77 edited register.php to accept more parameters

--- app/MQFunctions/register.php
+++ app/MQFunctions/register.php
@@ -1,12 +1,12 @@
 <?php
-function register($username, $password){
+function register($email, $username, $fName, $lName, $password){
 	try{
 		require_once(__DIR__.'/../../lib/path.inc');
 		require_once(__DIR__.'/../../lib/get_host_info.inc');
 		require_once(__DIR__.'/../../lib/rabbitMQLib.inc');
 
 		$client = new RabbitMQClient('testRabbitMQ.ini', 'testServer');
-		$msg = array("username"=>$username,"password"=>$password, "type"=>"register");
+		$msg = array("email"=>$email, "username"=>$username, "fName"=>$fName, "lName"=>$lName, "password"=>$password, "type"=>"register");
 		$response = $client->send_request($msg);
 		return $response;
 	}
