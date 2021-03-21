a1ba4351e1dea9eca90182abce14396cb89add61
Derek VanCaneghem <dmv7@njit.edu>
2021-03-21 04:17:41 -0400

    added product page and product list page, minor navbar changes and homepage changes

--- app/login.php
+++ app/login.php
@@ -1,129 +1,122 @@
 <?php
-require(__DIR__."/MQPublish.inc.php");
+require(__DIR__ . "/MQPublish.inc.php");
 //require(__DIR__."/header.php");
 session_start();
 
 
-if(isset($_POST["submit"])){
+if (isset($_POST["submit"])) {
 	$username = $_POST["username"];
 	$password = $_POST["password"];
 	//TODO validate
-	
+
 	ob_start();
 	//calls function from MQPublish.inc.php to communicate with MQ
 	$response = (array)login($username, $password);
 	ob_end_clean();
-	
-	if($response["status"] == 200){	
+
+	if ($response["status"] == 200) {
 		$_SESSION["user"] = $response["data"];
 		echo "<script>alert('Login Success')</script>";
 		echo "<script>window.location = 'index.php'; </script>";
-	}
-	else if ($response["status"] == 400){
+	} else if ($response["status"] == 400) {
 		echo "<script>alert('Login Failed')</script>";
 		echo "<script>window.location = 'login.php';</script>";
-	}
-	else if ($response["status"] == 403){
+	} else if ($response["status"] == 403) {
 		echo "<script>alert('Login Failed')</script>";
-                echo "<script>window.location = 'login.php';</script>";	
-	}
-	else {
+		echo "<script>window.location = 'login.php';</script>";
+	} else {
 		var_export($response);
 	}
-
 }
 ?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-<link rel="stylesheet" href="style.css">
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-<title>Login</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel="stylesheet" href="style.css">
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+	<title>Login</title>
 </head>
 
 <body>
-<!-- Nav -->
-<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-<nav class="navbar navbar-expand-md navbar-dark bg-dark">
-<div class="container">
-<a class="navbar-brand" href="index.php">GPU Guru</a>
-<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-<span class="navbar-toggler-icon"></span>
-</button>
+	<!-- Nav -->
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+	<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+		<div class="container">
+			<a class="navbar-brand" href="index.php">GPU Guru</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
 
-<div class="collapse navbar-collapse justify-content-end" id="navbarsExampleDefault">
-<ul class="navbar-nav m-auto">
-<li class="nav-item">
-<a class="nav-link" href="index.php">Home</a>
-</li>
-<li class="nav-item">
-<a class="nav-link" href="#">Products</a>
-</li>
-<li class="nav-item active">
-<a class="nav-link" href="login.php">Login</a>
-</li>
-<li class="nav-item">
-<a class="nav-link" href="register.php">Register</a>
-</li>
-<li class="nav-item">
-<a class="nav-link" href="#">Settings</a>
-</li>
-</ul>
+			<div class="collapse navbar-collapse justify-content-end" id="navbarsExampleDefault">
+				<ul class="navbar-nav m-auto">
+					<li class="nav-item">
+						<a class="nav-link" href="index.php">Home</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="productlist.php">Products</a>
+					</li>
+					<li class="nav-item active">
+						<a class="nav-link" href="login.php">Login</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="register.php">Register</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Settings</a>
+					</li>
+				</ul>
 
-<form class="form-inline my-2 my-lg-0">
-<div class="input-group input-group-sm">
-<input type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm" placeholder="Search...">
-<div class="input-group-append">
-<button type="button" class="btn btn-secondary btn-number">
-<i class="fa fa-search"></i>
-</button>
-</div>
-</div>
-</form>
-</div>
-</div>
-</nav>
-<!-- Login Form -->
-<form method="POST" action="login.php">
-<div class="container register-form">
-<div class="form">
-<div class="note">
-<p>Please fill in username and password to log in.</p>
-</div>
+				<form class="form-inline my-2 my-lg-0">
+					<div class="input-group input-group-sm">
+						<input type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm" placeholder="Search...">
+						<div class="input-group-append">
+							<button type="button" class="btn btn-secondary btn-number">
+								<i class="fa fa-search"></i>
+							</button>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+	</nav>
+	<!-- Login Form -->
+	<form method="POST" action="login.php">
+		<div class="container register-form">
+			<div class="form">
+				<div class="note">
+					<p>Please fill in username and password to log in.</p>
+				</div>
 
-<div class="form-content">
-<div class="row">
-<div class="col-md-12">
-<div class="form-group">
-<input type="text" class="form-control" placeholder="Enter Username" name="username" id="usr"
-required>
-</div>
-<div class="form-group">
-<input type="password" class="form-control" placeholder="Enter Password" name="password"
-id="psw" required>
-</div>
-<div class="theButton">
-<button type="submit" name="submit" class="btnSubmit">Login</button>
-</div>
+				<div class="form-content">
+					<div class="row">
+						<div class="col-md-12">
+							<div class="form-group">
+								<input type="text" class="form-control" placeholder="Enter Username" name="username" id="usr" required>
+							</div>
+							<div class="form-group">
+								<input type="password" class="form-control" placeholder="Enter Password" name="password" id="psw" required>
+							</div>
+							<div class="theButton">
+								<button type="submit" name="submit" class="btnSubmit">Login</button>
+							</div>
 
-<div class="nextPage">
-<br>
-<p>Do not have an account? <a href="register.php">Register</a>.</p>
-</div>
-</div>
-</div>
-</div>
-</div>
-</div>
-</div>
-</form>
+							<div class="nextPage">
+								<br>
+								<p>Do not have an account? <a href="register.php">Register</a>.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		</div>
+	</form>
 </body>
+
 </html>
-
-
