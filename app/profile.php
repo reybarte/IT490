d661bd6b3cd37734db292e851b6a9a316cba9b1d
PaulJQ <pauljq48@gmail.com>
2021-04-07 09:32:34 +0000

    profile.php page drafted (does not show info, just basis for how it looks.)

---
+++ app/profile.php
@@ -0,0 +1,91 @@
+<?php
+session_start();
+require(__DIR__ . "/MQPublish.inc.php");
+require(__DIR__ . "/header.php");
+ob_start();
+
+$data = getCache()->data;
+ob_end_clean();
+foreach ($data as $key => $value) {
+    $asinData[strval($value->asin)] = (array)$value;
+}
+//var_dump($asinData);
+foreach ($asinData as $key => $value) {
+    //echo $key . ":" . $value["current_price"]."\n";
+}
+?>
+<!DOCTYPE html>
+<html lang=en>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="CSS/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="JS/desclimit.js"></script> <!-- Add 'limitdesc' as a class variable to limit characters -->
+    <link rel="icon" href="IMG/490IconPic.png">
+    <title>Profile</title>
+</head>
+
+<body>
+    <div class="container d-flex justify-content-center mt-50 mb-50">
+        <div class="row justify-content-center">
+            <div class="col-md-10">
+                <!--Page container-->
+                <div class="card card-body">
+                    <!--Header container-->
+                    <div class="media align-items-center align-items-lg-start text-center text-lg-left flex-column flex-lg-row">
+                        <div class="media-body">
+                            <h6 class="media-title font-weight-semibold"> <a>Profile</a></h6>
+                            <p class="mb-1"></p>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="card card-body">
+                    <!--Name/email container-->
+                    <div class="col-12 col-md-6 col-lg-4 pb-3">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title text-center"><a title="View Product">Email</a></h4>
+                                <p class="card-text limitdesc text-center"></p>
+                                <div class="row">
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card card-body">
+                    <!--Points container-->
+                    <div class="col-12 col-md-6 col-lg-4 pb-3">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title text-center"><a title="View Product">Point Balance</a></h4>
+                                <p class="card-text limitdesc text-center"></p>
+                                <div class="row">
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card card-body">
+                    <!--Tracking container-->
+                    <div class="media align-items-center align-items-lg-start text-center text-lg-left flex-column flex-lg-row">
+                        <div class="media-body">
+                            <h6 class="media-title font-weight-semibold"> <a>Tracking Preferences</a></h6>
+                            <p class="mb-1"></p>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
