54b97a5c111570127475fe3691c66165d11f4838
tll5 <tll5@njit.edu>
2021-04-12 18:19:04 +0000

    changed the header file to fix issue

--- app/header.php
+++ app/header.php
@@ -19,12 +19,22 @@ $clientRole = $_SESSION["user"]["role"] == 'client' ? 1 : 0;
 	.dropdown-menu {
 		margin-top: 0 !important;
 	}
+
+	/*Image settings for small logo in navbar*/
+	.logo-image {
+		width: 50px;
+		height: 50px;
+	}
 </style>
 
 <!-- NavBar -->
 <nav class="navbar navbar-expand-md navbar-dark bg-dark">
 	<div class="container-fluid">
-		<a class="navbar-brand" href="index.php">GPU Guru</a>
+
+		<div class="logo-image mr-1">
+			<a href="<?php echo $prodPage ? '../index.php' : 'index.php' ?>"> <img src="<?php echo $prodPage ? '../IMG/490IconPic.png' : 'IMG/490IconPic.png' ?>" class="img-fluid"></a>
+		</div>
+
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
@@ -34,20 +44,27 @@ $clientRole = $_SESSION["user"]["role"] == 'client' ? 1 : 0;
 				<li class="nav-item <?php echo $currentURL['index'] ? 'active' : '' ?> ">
 					<a class="nav-link" href="<?php echo $prodPage ? '../index.php' : 'index.php' ?>">Home</a>
 				</li>
-				<li class="nav-item <?php echo $currentURL['productlist'] ? 'active' : '' ?> ">
-					<a class="nav-link" href="<?php echo $prodPage ? '../productlist.php' : 'productlist.php' ?>">Products</a>
+				<li class="nav-item <?php echo $currentURL['prodList'] ? 'active' : '' ?> ">
+					<a class="nav-link" href="<?php echo $prodPage ? '../prodList.php' : 'prodList.php' ?>">Products</a>
 				</li>
-				<li class="nav-item <?php echo $currentURL['statistics'] ? 'active' : '' ?> ">
-					<a class="nav-link" href="<?php echo $prodPage ? '../statistics.php' : 'statistics.php' ?>">Statistics</a>
+				<li class="nav-item <?php echo $currentURL['tracking'] ? 'active' : '' ?> ">
+					<a class="nav-link" href="<?php echo $prodPage ? '../tracking.php' : 'tracking.php' ?>">Tracking</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Statistics</a>
 				</li>
 				<div class="nav-item dropdown ml-auto">
 					<a href="#" class="nav-link dropdown-toggle <?php echo ($currentURL['login'] || $currentURL['register'] || $currentURL['admin']) ? 'active' : '' ?>" data-toggle="dropdown">Profile</a>
 					<div class="dropdown-menu">
 						<?php if ($loggedIn) : ?>
+							<a href="<?php echo $prodPage ? '../profile.php' : 'profile.php' ?>" class="dropdown-item <?php echo $currentURL['profile'] ? 'active' : '' ?> ">Profile</a>
+						<?php endif; ?>
+						<?php if ($loggedIn) : ?>
 							<a href="#" class="dropdown-item">History</a>
 						<?php endif; ?>
 						<?php if ($loggedIn) : ?>
-							<a href="#" class="dropdown-item">Logout</a>
+							<a href="<?php echo $prodPage ? '../login.php' : 'login.php' ?>" class="dropdown-item <?php echo $currentURL['login'] ? 'active' : '' ?> ">Logout</a>
+							<?php $_SESSION["logout"] = true; ?>
 						<?php endif; ?>
 						<?php if (!$loggedIn) : ?>
 							<a href="<?php echo $prodPage ? '../login.php' : 'login.php' ?>" class="dropdown-item <?php echo $currentURL['login'] ? 'active' : '' ?> ">Login</a>
