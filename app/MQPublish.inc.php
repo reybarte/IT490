6b78b6e24127474b30a4e8a5e915b9ea65d8f213
michaeltome <mtt24@njit.edu>
2021-04-14 05:07:28 +0000

    committing changes to update DB

--- app/MQPublish.inc.php
+++ app/MQPublish.inc.php
@@ -3,16 +3,16 @@
 //the purpose of this is to give those with minimal PHP knowledge a way
 //to divide up code and assign to different team members without having too many hurdles
 
-require(__DIR__."/MQFunctions/login.php");
-require(__DIR__."/MQFunctions/register.php");
-require(__DIR__."/MQFunctions/apiCall.php");
-require(__DIR__."/MQFunctions/roleChange.php");
-require(__DIR__."/MQFunctions/getCache.php");
-require(__DIR__."/MQFunctions/logout.php");
-require(__DIR__."/MQFunctions/transaction.php");
-require(__DIR__."/MQFunctions/remove.php");
-require(__DIR__."/MQFunctions/tracking.php");
-require(__DIR__."/MQFunctions/getTrackingInfo.php");
-
+require(__DIR__ . "/MQFunctions/login.php");
+require(__DIR__ . "/MQFunctions/register.php");
+require(__DIR__ . "/MQFunctions/apiCall.php");
+require(__DIR__ . "/MQFunctions/roleChange.php");
+require(__DIR__ . "/MQFunctions/getCache.php");
+require(__DIR__ . "/MQFunctions/logout.php");
+require(__DIR__ . "/MQFunctions/transaction.php");
+require(__DIR__ . "/MQFunctions/remove.php");
+require(__DIR__ . "/MQFunctions/tracking.php");
+require(__DIR__ . "/MQFunctions/getTrackingInfo.php");
+require(__DIR__ . "/MQFunctions/getTransactionHistory");
 //TODO include other functions here as they're developed
 //TODO include only this file when you need to use any of the functions
