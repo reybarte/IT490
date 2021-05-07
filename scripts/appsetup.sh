8e9bfa0d50ce9a6f185748bb22e8d20970908a15
reybarte <reynaldbarte@gmail.com>
2021-05-07 09:23:57 -0400

    app set up tested and functioning

--- scripts/appsetup.sh
+++ scripts/appsetup.sh
@@ -10,13 +10,14 @@ sudo apt update -y
 sudo apt upgrade -y
 #install apache server
 sudo apt install apache2 -y
+sudo service apache2 start
 #install php modules
 sudo apt install php7.4 -y
 sudo apt install composer -y
 
 sudo mkdir repo
 sudo rm /var/www/html/index.html
-sudo ln -s repo /var/www/html/repo
-sduo chmod 777 /var/www/html -R
+sudo ln -s ~/repo /var/www/html/
+sudo chmod 777 /var/www/html
 #manually edit /etc/apache2/sites-available/000-default.conf
 #DocumentRoot /var/www/html/repo/app
